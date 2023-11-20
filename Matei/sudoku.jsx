7de32dee5a2e1c5eba08131b2275d229dc45103f
Martin Matei <martin.matei@gmail.com>
2023-11-20 22:43:08 +0200

    made the api work

--- Matei/sudoku.jsx
+++ Matei/sudoku.jsx
@@ -1,12 +1,73 @@
-import React from "react";
-import { useRef, useEffect, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import axios from "axios";
 
 const Sudoku = () => {
 
-    return(
-        <h1>This is sudoku</h1>
-    )
-};
+    const fileInput = useRef();
+    const [data, setData] = useState(null);
 
+    const handleFileChange = (event) => {
+        if (event.target.files.length > 0) {
+            const file = event.target.files[0];
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                const base64String = reader.result
+                    .replace("data:", "")
+                    .replace(/^.+,/, "");
+
+                const img = new Image();
+                img.onload = function () {
+                    // Log the image size
+                    console.log('Image Width: ' + this.width + ', Image Height: ' + this.height);
+                };
+                img.src = reader.result;
+
+                axios({
+                    method: "POST",
+                    url: "https://detect.roboflow.com/sodoku-2/1",
+                    params: {
+                        api_key: "zFBhhnpseoexwI9vK8LJ",
+                    },
+                    data: base64String,
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                })
+                    .then(function (response) {
+                        console.log(response.data);
+                        setData(response.data);
+                    })
+                    .catch(function (error) {
+                        console.log(error.message);
+                    });
+            };
+
+
+            reader.readAsDataURL(file);
+            fileInput.current.value = "";
+        }
+    };
+
+
+
+    return (
+        <div>
+            <input type="file" id="Form" ref={fileInput} onChange={handleFileChange} />
+            {(data !== null && data?.predictions.length === 81) && (
+
+                <h1>Is ok</h1>
+
+            )}
+            {(data !== null && data?.predictions.length !== 81) && (
+
+
+                <h1>Not ok</h1>
+
+            )}
+        </div>
+    );
+
+};
 
 export default Sudoku;
