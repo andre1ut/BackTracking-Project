362ce3674a4d3fb9e4ef1d581207cb52e0db7019
Martin Matei <martin.matei@gmail.com>
2023-11-21 22:36:44 +0200

    remouved the home button

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,11 @@ const App = () => {
 
     const [route, setRoute] = useState("home");
 
-   
+
     return (
         <div>
-            <button onClick={() => setRoute("home")}>Home</button>
             <button onClick={() => setRoute("Matei")}>Matei</button>
             <button onClick={() => setRoute("Andrei")}>Andrei</button>
-            {route === "home" ? <h1>Home</h1> : null}
             {route === "Matei" ? <Sudoku /> : null}
             {route === "Andrei" ? <h1>Andrei</h1> : null}
         </div>
