43ceeb084682350056334b97eb5009811457a5fa
andre1ut <andrei.dumitru-munteanu@cnmv.com>
2023-12-12 10:30:47 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/BackTracking-Project/',
   optimizeDeps: {
     exclude: ['fsevents'],
-  }, 
-  base: '/BackTracking-Project/'
+  },
 })
